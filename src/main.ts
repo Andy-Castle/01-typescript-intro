a12f84e448681dccb80bc760253867c2af3c4317
Andy Castle <masterchief_and@live.com>
2025-01-17 12:56:13 -0500

    Esto es un ejercicio de como desestructurar las funciones

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import "./style.css";
 // import "./Topics/02-object-interface";
 // import "./Topics/03-functions";
 // import "./Topics/04-homework-types";
-import "./Topics/05-basic-destructuring";
+// import "./Topics/05-basic-destructuring";
+import "./Topics/06-function-destructuring";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   Hola Mundo
