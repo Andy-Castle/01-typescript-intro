b2217e71682dd9ab899eceac909164304379e7b6
Andy Castle <masterchief_and@live.com>
2025-01-17 13:54:21 -0500

    se vio como priorizar composicion sobre herencia y que es util para reutilizar codigo

--- src/Topics/08-classes.ts
+++ src/Topics/08-classes.ts
@@ -2,19 +2,36 @@ export class Person {
   // public name: string;
   // private address: string;
 
-  constructor(public name: string, private address: string = "No Address") {}
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    private address: string = "No Address"
+  ) {}
 }
 
-export class Hero extends Person {
-  constructor(public alterEgo: string, public age: number, public realName: string) {
-    super(realName, "New York");
-  }
+// export class Hero extends Person {
+//   constructor(public alterEgo: string, public age: number, public realName: string) {
+//     super(realName, "New York");
+//   }
+// }
+
+export class Hero {
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string,
+    public person: Person
+  ) {}
 }
 
-const ironman = new Hero("IronMan", 45, "Tony");
+const tony = new Person("Tony ", "Stark", "New York");
+
+const ironman = new Hero("IronMan", 45, "Tony", tony);
 
 console.log(ironman);
 
 //Constructor de una clase
 
 //Extender una clase
+
+//Priorizar composición sobre herencia
