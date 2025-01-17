8592cc6c0184552e6223fa262393c2dc087b2ee7
Andy Castle <masterchief_and@live.com>
2025-01-17 13:45:30 -0500

    Se vio como extender una clase con ts

--- src/Topics/08-classes.ts
+++ src/Topics/08-classes.ts
@@ -5,8 +5,16 @@ export class Person {
   constructor(public name: string, private address: string = "No Address") {}
 }
 
-const ironman = new Person("IronMan", "New York");
+export class Hero extends Person {
+  constructor(public alterEgo: string, public age: number, public realName: string) {
+    super(realName, "New York");
+  }
+}
+
+const ironman = new Hero("IronMan", 45, "Tony");
 
 console.log(ironman);
 
 //Constructor de una clase
+
+//Extender una clase
